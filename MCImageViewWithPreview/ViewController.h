735143177b3936af99148295dabd9efede641489
Baglan Dosmagambetov <baglan.dos@gmail.com>
2012-10-14 18:04:11 +0600

    Initial Commit

---
+++ MCImageViewWithPreview/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MCImageViewWithPreview
+//
+//  Created by Baglan on 10/14/12.
+//  Copyright (c) 2012 MobileCreators. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
